5ff073d976d71e93f01b58b30663e12d3c9455fe
ritiksharmarj <theonlyritik@gmail.com>
2023-03-10 17:06:57 +0530

    API: Handling GET Requests

---
+++ app.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const express = require('express');
+const app = express();
+
+const tours = JSON.parse(
+   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+   res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+         tours,
+      },
+   });
+});
+
+app.listen(8000, () => {
+   console.log('App running');
+});
