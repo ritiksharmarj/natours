3880666ab6b3fe7151e39d3174ef894ef9bedbc0
ritiksharmarj <theonlyritik@gmail.com>
2023-05-07 14:53:54 +0530

    morgan middleware

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import { readFileSync, writeFile } from 'fs';
 import express from 'express';
+import morgan from 'morgan';
 
 const app = express();
 
 // Middleware
+app.use(morgan('dev'));
 app.use(express.json());
 
 // Read the tours file JSON data
