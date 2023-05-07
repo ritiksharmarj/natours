c94584837b567f20142dd3b824fcd3cb26858b61
ritiksharmarj <theonlyritik@gmail.com>
2023-05-07 15:16:21 +0530

    implementing the users routes

--- app.js
+++ app.js
@@ -118,6 +118,42 @@ const deleteTour = (req, res) => {
   );
 };
 
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+
+// Implementing the "tours" routes
 app.route('/api/v1/tours').get(getAllTours).post(createTour);
 
 app
@@ -125,6 +161,15 @@ app
   .get(getTour)
   .patch(updateTour)
   .delete(deleteTour);
+
+// Implementing the "users" routes
+app.route('/api/v1/users').get(getAllUsers).post(createUser);
+
+app
+  .route('/api/v1/users/:id')
+  .get(getUser)
+  .patch(updateUser)
+  .delete(deleteUser);
 
 // Create a server on 127.0.0.1:8000
 app.listen(8000, () => {
