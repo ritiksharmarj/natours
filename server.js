5beef6c872878fa2131db6c65466a9dcbb681b63
ritiksharmarj <theonlyritik@gmail.com>
2023-05-09 01:32:06 +0530

    connecting database to express app

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
+
 dotenv.config({ path: './.env' });
 
-const app = require('./app');
+// MongoDB database connection
+const uri = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB Connection Successful 🖐'));
 
 // Create a server on 127.0.0.1:8000
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log('Server started 🖐');
+  console.log(`Server started ${port} 🖐`);
 });
