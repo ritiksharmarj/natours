0132537946ccbea3796ea43363545c6661995f0c
ritiksharmarj <theonlyritik@gmail.com>
2023-05-09 11:21:56 +0530

    simple tour model

--- server.js
+++ server.js
@@ -17,6 +17,26 @@ mongoose
   })
   .then(() => console.log('DB Connection Successful 🖐'));
 
+// Defining schema
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'], // specify the error that we want to be displayed when we're missing this field.
+    unique: true, // we can't have two tour documents with the same name.
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+// Creating a model
+const Tour = mongoose.model('Tour', tourSchema);
+
 // Create a server on 127.0.0.1:8000
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
