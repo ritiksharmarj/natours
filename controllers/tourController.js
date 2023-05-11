8d862b4477b1a1c26d9f0d9130e9f7a01f78da9a
ritiksharmarj <theonlyritik@gmail.com>
2023-05-11 17:29:38 +0530

    refactoring api features

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,5 @@
 const Tour = require('../models/tourModel');
+const APIFeatures = require('../utils/apiFeatures');
 
 /**
  * @middleware - aliasTopTours
@@ -18,70 +19,13 @@ exports.aliasTopTours = (req, res, next) => {
  */
 exports.getAllTours = async (req, res) => {
   try {
-    /////////////////////////////////
-    // 1A - Filtering: extract filters from query string
-    const queryObj = { ...req.query };
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach((el) => delete queryObj[el]);
-
-    /////////////////////////////////
-    // 1B - Advanced filtering
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(
-      /\b(gt|gte|lt|lte|eq|ne)\b/g,
-      (match) => `$${match}`
-    ); // queryStr: { 'duration': { '$gte': '3' }}
-
-    // Build query
-    let query = Tour.find(JSON.parse(queryStr));
-
-    /////////////////////////////////
-    // 2 - Sorting
-    if (req.query.sort) {
-      /**
-       * /api/v1/tours?sort=price,-ratingsAverage
-       * sorting the tours by "price" in ascending order
-       * "ratingsAverage" in descending order (denoted by the "-" before "ratingsAverage")
-       *
-       * equivalent - query.sort('price -ratingsAverage');
-       */
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy);
-    } else {
-      // If sort field empty then results should be sorted in descending order to show the newest document first.
-      query = query.sort('-createdAt');
-    }
-
-    /////////////////////////////////
-    // 3 - Field limiting
-    if (req.query.fields) {
-      /**
-       * /api/v1/tours?fields=name,duration,difficulty,price
-       * In this case, the response will only include the "name", "duration", "difficulty", and "price" fields, rather than returning all the fields of the tours resource.
-       *
-       * equivalent - query.select('name duration difficulty price');
-       */
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    } else {
-      query = query.select('-__v');
-    }
-
-    /////////////////////////////////
-    // 3 - Pagination
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-
-    query = query.skip(skip).limit(limit);
-
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) throw new Error('This page does not exist');
-    }
-
     // Execute query
-    const tours = await query;
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
 
     // Send response
     res.status(200).json({
---
+++ utils/apiFeatures.js
@@ -0,0 +1,73 @@
+class APIFeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+
+  filter() {
+    // 1A - Filtering: extract filters from query string
+    const queryObj = { ...this.queryString };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    // 1B - Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(
+      /\b(gt|gte|lt|lte|eq|ne)\b/g,
+      (match) => `$${match}`
+    ); // queryStr: { 'duration': { '$gte': '3' }}
+
+    // Build query
+    this.query = this.query.find(JSON.parse(queryStr));
+
+    return this;
+  }
+
+  sort() {
+    if (this.queryString.sort) {
+      /**
+       * /api/v1/tours?sort=price,-ratingsAverage
+       * sorting the tours by "price" in ascending order
+       * "ratingsAverage" in descending order (denoted by the "-" before "ratingsAverage")
+       *
+       * equivalent - query.sort('price -ratingsAverage');
+       */
+      const sortBy = this.queryString.sort.split(',').join(' ');
+      this.query = this.query.sort(sortBy);
+    } else {
+      // If sort field empty then results should be sorted in descending order to show the newest document first.
+      this.query = this.query.sort('-createdAt');
+    }
+
+    return this;
+  }
+
+  limitFields() {
+    if (this.queryString.fields) {
+      /**
+       * /api/v1/tours?fields=name,duration,difficulty,price
+       * In this case, the response will only include the "name", "duration", "difficulty", and "price" fields, rather than returning all the fields of the tours resource.
+       *
+       * equivalent - query.select('name duration difficulty price');
+       */
+      const fields = this.queryString.fields.split(',').join(' ');
+      this.query = this.query.select(fields);
+    } else {
+      this.query = this.query.select('-__v');
+    }
+
+    return this;
+  }
+
+  paginate() {
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    this.query = this.query.skip(skip).limit(limit);
+
+    return this;
+  }
+}
+
+module.exports = APIFeatures;
