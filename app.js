b707ceedd2093d0a4022249792938a80db9a39d1
ritiksharmarj <theonlyritik@gmail.com>
2023-05-25 15:40:11 +0530

    set security http headers

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -10,20 +11,27 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 /////////////////////////////////
-// GLOBAL MIDDLEWARES
+// 1) GLOBAL MIDDLEWARES
+// Helmet helps secure Express apps by setting HTTP response headers.
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Apply the rate limiting middleware to API calls only
 const apiLimiter = rateLimit({
   windowMs: 60 * 60 * 1000, // 1 hour
   max: 100, // Limit each IP to 100 requests per `window` (here, per 1 hour)
   message: 'Too many requests from this IP, please try again after an hour!',
 });
-// Apply the rate limiting middleware to API calls only
 app.use('/api', apiLimiter);
 
-app.use(express.json()); // to accept JSON data
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
 // Test middleware
@@ -34,16 +42,12 @@ app.use(express.static(`${__dirname}/public`));
 // });
 
 /////////////////////////////////
-// ROUTES
+// 2) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 // Handling unhandled routes - routes which we haven't defined yet e.g. /api/tours
 app.all('*', (req, res, next) => {
-  // const err = new Error(`Can't find ${req.originalUrl} on this server!`);
-  // err.status = 'fail';
-  // err.statusCode = 404;
-
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
