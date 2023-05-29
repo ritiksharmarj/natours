4ad5764e60d13451b9c51f5c58eec24c554720ea
ritiksharmarj <theonlyritik@gmail.com>
2023-05-29 14:13:10 +0530

    populating reviews

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -33,6 +33,19 @@ const reviewSchema = new mongoose.Schema(
   }
 );
 
+// QUERY MIDDLEWARE
+reviewSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'user',
+    select: 'name photo',
+  }).populate({
+    path: 'tour',
+    select: 'name',
+  });
+
+  next();
+});
+
 const Review = mongoose.model('Review', reviewSchema);
 
 module.exports = Review;
