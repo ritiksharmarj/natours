f8d7a1f30fdcd5d1f233f6ea0b1637dcba19f839
ritiksharmarj <theonlyritik@gmail.com>
2023-06-10 17:41:01 +0530

    Log in feature

--- public/js/alerts.js
+++ public/js/alerts.js
@@ -1,1 +1,19 @@
-console.log('Hello 🖐');
+export const hideAlert = () => {
+  const el = document.querySelector('.alert');
+  if (el) el.parentElement.removeChild(el);
+};
+
+/**
+ * @param {String} type either "success" or "error"
+ * @param {String} message alert message
+ */
+export const showAlert = (type, message) => {
+  // first hide all the alerts that already exists.
+  hideAlert();
+
+  const markup = `<div class="alert alert--${type}">${message}</div>`;
+  document.querySelector('body').insertAdjacentHTML('afterbegin', markup);
+
+  // hide all the alerts after 5 seconds.
+  window.setTimeout(hideAlert, 5000);
+};
--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,24 @@
-console.log('Hello from parcel 🖐');
+import { displayMap } from './mapbox';
+import { login } from './login';
+
+// DOM ELEMENTS
+const mapBox = document.getElementById('map');
+const loginForm = document.querySelector('.form');
+
+// DELEGATION
+if (mapBox) {
+  const locations = JSON.parse(mapBox.dataset.locations);
+  displayMap(locations);
+}
+
+if (loginForm) {
+  // Getting email and password from "/login" form
+  loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    login(email, password);
+  });
+}
