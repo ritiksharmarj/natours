35057a9f0d4c3a9db4648bc4a151afde76d224a6
ritiksharmarj <theonlyritik@gmail.com>
2023-06-12 16:39:14 +0530

    adding image uploads to form

--- controllers/userController.js
+++ controllers/userController.js
@@ -80,9 +80,6 @@ exports.getMe = (req, res, next) => {
  * @route - PATCH /api/v1/users/updateMe
  */
 exports.updateMe = catchAsync(async (req, res, next) => {
-  console.log('Body: ', req.body);
-  console.log('File: ', req.file);
-
   // 1) Create error if user POSTs password data
   if (req.body.password || req.body.passwordConfirm) {
     return next(
--- public/js/index.js
+++ public/js/index.js
@@ -33,10 +33,12 @@ if (userDataForm) {
   userDataForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
+    const form = new FormData();
+    form.append('name', document.getElementById('name').value);
+    form.append('email', document.getElementById('email').value);
+    form.append('photo', document.getElementById('photo').files[0]);
 
-    updateSettings({ name, email }, 'data');
+    updateSettings(form, 'data');
   });
 }
 
