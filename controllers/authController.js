7b57803dcafd60c30bb0d51438f0c2fadd947a29
ritiksharmarj <theonlyritik@gmail.com>
2023-06-17 00:45:47 +0530

    implemented cors

--- controllers/authController.js
+++ controllers/authController.js
@@ -19,8 +19,11 @@ const createSendToken = (user, statusCode, req, res) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
+    secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
   };
-  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  // if (req.secure || req.headers['x-forwarded-proto'] === 'https') cookieOptions.secure = true;
+
   res.cookie('jwt', token, cookieOptions);
 
   // res.cookie('jwt', token, {
