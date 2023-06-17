a159e68970ba26f71f2d32d71660011ba0c971bd
ritiksharmarj <theonlyritik@gmail.com>
2023-06-17 17:44:36 +0530

    fixes webhook

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -54,10 +54,9 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 });
 
 const createBookingCheckout = async (session) => {
-  console.log(session);
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_items[0].price_data.unit_amount / 100;
+  const price = session.amount_total / 100;
 
   await Booking.create({ tour, user, price });
 };
@@ -86,8 +85,6 @@ exports.webhookCheckout = (req, res, next) => {
   // Handle the event
   if (event.type === 'checkout.session.completed')
     createBookingCheckout(event.data.object);
-
-  console.log(event);
 
   res.status(200).json({ received: true });
 };
